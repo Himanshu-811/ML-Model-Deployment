4a8e166c1ef809a455f66fd54ee4a91646668636
Himanshu Kumar <himanshu.college.811@gmail.com>
2026-01-30 12:43:20 +0530

    Add Iris Classifier app with Streamlit interfae

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_model():
 model = load_model()
 
 # UI INTERFACE
-st.title("🌸 Iris Species Predictor")
+st.title("🌸 Iris Species Predictor 🌸")
 st.markdown("""This app uses a **Logistic Regression**  model to predict the species of an Iris flower based on its physical measurements.""")
 
 # sidebar for user inputs
